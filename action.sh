00e279493e677e0f06b385be5692dd876915034d
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-01-24 17:25:29 +0900

    feat: change the default install path
    Change the default install path from `/usr/local/bin/aqua` to `${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin/aqua`
    
    The root privilege isn't needed.

---
+++ action.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+if [ -n "${AQUA_INSTALL_PATH:-}" ]; then
+	"$GITHUB_ACTION_PATH/aqua-installer" -v "${AQUA_VERSION}" -i "${AQUA_INSTALL_PATH}"
+	# shellcheck disable=SC2086
+	"${AQUA_INSTALL_PATH}" i $AQUA_OPTS
+	exit 0
+fi
+
+"$GITHUB_ACTION_PATH/aqua-installer" -v "${AQUA_VERSION}"
+# shellcheck disable=SC2086
+aqua i $AQUA_OPTS
